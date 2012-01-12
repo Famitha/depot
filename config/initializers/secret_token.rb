db993e48b287f0dcf83b701e6eb44550398939e9
famithabanu <famitha@railsfactory.org>
2012-01-12 18:34:50 +0530

    product scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '8b9bb93b7f094ae41e3f251eaab165a22aaf5171c847cfb27d27eab7f8e0068f69026b802fb18b74e2b8ad1a0a3590f364d4ec38eb18fd50c7616906c7f672c7'
