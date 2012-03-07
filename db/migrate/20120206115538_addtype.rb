7f569d5aaa477c8aab11f1ecae9db0b0a67534cc
Famitha <fami008@gmail.com>
2012-03-07 17:52:29 +0530

    simple captcha

--- db/migrate/20120206115538_addtype.rb
+++ db/migrate/20120206115538_addtype.rb
@@ -1,6 +1,6 @@
 class Addtype < ActiveRecord::Migration
   def up
-     add_column (:users, :type, :boolean, :default => false)
+     add_column :users, :type, :boolean, :default => false
   end
 
   def down
